bddbc291c782956919a7d31cb37fab78275dc829
unknown <sami.t.arar@hotmail.com>
2020-09-05 18:49:45 -0400

    Enabled scrollreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,24 +27,28 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
-/*const sr = ScrollReveal({
+const sr = ScrollReveal({
   origin: "top",
   distance: "80px",
   duration: 1000,
   reset: false,
-}); */
+});
 
 /*SCROLL HOME*/
 sr.reveal(".home__title", {});
 sr.reveal(".home__title-occupation", {});
-sr.reveal(".button", { delay: 200 });
+sr.reveal(".button", { delay: 300 });
+sr.reveal(".home__download-cv", { delay: 200 });
 sr.reveal(".home__img", { delay: 400 });
 sr.reveal(".home__social-icon", { interval: 200 });
 
 /*SCROLL ABOUT*/
 sr.reveal(".about__img", {});
-sr.reveal(".about__subtitle", { delay: 400 });
-sr.reveal(".about__text", { delay: 400 });
+sr.reveal(".about__text", { delay: 100 });
+sr.reveal(".about__subtitle", { delay: 100 });
+sr.reveal(".about__row", { delay: 200 });
+sr.reveal(".about__location", { delay: 200 });
+sr.reveal(".about__description", { delay: 200 });
 
 /*SCROLL SKILLS*/
 sr.reveal(".skills__subtitle", {});
@@ -53,10 +57,13 @@ sr.reveal(".skills__data", { interval: 200 });
 sr.reveal(".skills__img", { delay: 600 });
 
 /*SCROLL PROJECTS*/
+sr.reveal(".projects__subtitle", {});
 sr.reveal(".projects__img", { interval: 200 });
+sr.reveal(".projects__skills", { interval: 160 });
 
 /*SCROLL CONTACT*/
-sr.reveal(".contact__container", { interval: 200 });
+sr.reveal(".contact__social-icon", { interval: 200 });
+sr.reveal(".contact__social-icon-location", { delay: 600 });
 
 function confirmDownloadResume() {
   if (confirm("Download the Resume in PDF format?")) {
